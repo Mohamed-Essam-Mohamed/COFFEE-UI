7a032d8f4e7a009b88cf59beb175472ee02dc82d
mohamed <esammww.com@gmail.com>
2024-05-15 03:14:25 +0300

    init app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:coffe_ui/src/feature/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CoffeeShop());
+}
+
+class CoffeeShop extends StatelessWidget {
+  const CoffeeShop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
