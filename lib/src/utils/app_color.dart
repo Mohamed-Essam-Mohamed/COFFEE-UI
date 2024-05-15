1cae263610a1bafe58b4d6de93f46ddd8baca802
mohamed <esammww.com@gmail.com>
2024-05-15 03:57:13 +0300

    finish Bottom Navigation

---
+++ lib/src/utils/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color colorBlack = Colors.black;
+  static const Color colorOrange = Colors.orange;
+  static const Color colorGrey = Colors.grey;
+}
